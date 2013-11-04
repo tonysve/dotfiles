ace70c47a3749c10ca1d4d7d266f75fdc2c80d22
Michael Aderskog <michael.aderskog@gmail.com>
2013-11-05 00:22:57 +0100

    attempt to handle dependency for oh-my-zsh

--- setlinks.sh
+++ setlinks.sh
@@ -3,6 +3,8 @@
 ln -nfs ~/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/dotfiles/vim/vim ~/.vim
 
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/dotfiles/zsh/oh-my-zsh
+
 ln -nfs ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -nfs ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
 
