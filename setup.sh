29f8a65247a3324a80623ff4c825b57fe21083b4
Michael Aderskog <michael.aderskog@gmail.com>
2013-11-06 12:59:30 +0100

    add zsh syntax higlighting plugin

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+# Setup for vim
 ln -nfs ~/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/dotfiles/vim/vim ~/.vim
 
+# Setup for zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/dotfiles/zsh/oh-my-zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/dotfiles/zsh/custom/plugins/zsh-syntax-highlighting
 
 ln -nfs ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -nfs ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
